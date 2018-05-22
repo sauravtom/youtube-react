e508071f4b07cba260e4ef57bd31565653591896
Saurav Tomar <saurav@chaintope.com>
2018-05-22 18:44:24 +0800

    final state

---
+++ src/components/search_bar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+//functional react component
+//cannot have state
+const SearchBar_ = () => {
+	return <input />;
+}
+
+// react class component with state
+class SearchBar extends React.Component {
+
+	// state declaration
+	constructor(props){
+		super(props);
+		this.state = {term: ''};
+	}
+
+	render() {
+		return (
+			<div className="search-bar">
+				<input
+					value = {this.state.term} 
+					onChange={event => this.onInputChange(event.target.value) }/>
+			</div>
+		);
+	}
+
+	onInputChange(term){
+		this.setState( {term} );
+		this.props.onSearchTermChange(term);
+		console.log(term);
+	}
+	
+}
+
+export default SearchBar;
